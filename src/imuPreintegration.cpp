2ed3b2aa71629f03b0ead221e8ec765910e4118e
epfl-xplore <se.software@epfl-xplore.ch>
2025-02-28 22:31:08 +0100

    removed debug prins

--- src/imuPreintegration.cpp
+++ src/imuPreintegration.cpp
@@ -329,16 +329,16 @@ public:
             graphFactors.add(priorBias);
 
             //debug logging
-            RCLCPP_INFO(
-                get_logger(),
-                "IMU INIT: Accel Bias (x: %f, y: %f, z: %f), Gyro Bias (x: %f, y: %f, z: %f)",
-                prevBias_.accelerometer().x(),
-                prevBias_.accelerometer().y(),
-                prevBias_.accelerometer().z(),
-                prevBias_.gyroscope().x(),
-                prevBias_.gyroscope().y(),
-                prevBias_.gyroscope().z()
-            );
+            // RCLCPP_INFO(
+            //     get_logger(),
+            //     "IMU INIT: Accel Bias (x: %f, y: %f, z: %f), Gyro Bias (x: %f, y: %f, z: %f)",
+            //     prevBias_.accelerometer().x(),
+            //     prevBias_.accelerometer().y(),
+            //     prevBias_.accelerometer().z(),
+            //     prevBias_.gyroscope().x(),
+            //     prevBias_.gyroscope().y(),
+            //     prevBias_.gyroscope().z()
+            // );
 
             // add values
             graphValues.insert(X(0), prevPose_);
@@ -437,16 +437,16 @@ public:
         prevBias_  = result.at<gtsam::imuBias::ConstantBias>(B(key));
 
             //debug logging
-        RCLCPP_INFO(
-            get_logger(),
-            "IMU INIT OPTI: Accel Bias (x: %f, y: %f, z: %f), Gyro Bias (x: %f, y: %f, z: %f)",
-            prevBias_.accelerometer().x(),
-            prevBias_.accelerometer().y(),
-            prevBias_.accelerometer().z(),
-            prevBias_.gyroscope().x(),
-            prevBias_.gyroscope().y(),
-            prevBias_.gyroscope().z()
-        );
+        // RCLCPP_INFO(
+        //     get_logger(),
+        //     "IMU INIT OPTI: Accel Bias (x: %f, y: %f, z: %f), Gyro Bias (x: %f, y: %f, z: %f)",
+        //     prevBias_.accelerometer().x(),
+        //     prevBias_.accelerometer().y(),
+        //     prevBias_.accelerometer().z(),
+        //     prevBias_.gyroscope().x(),
+        //     prevBias_.gyroscope().y(),
+        //     prevBias_.gyroscope().z()
+        // );
         // Reset the optimization preintegration object.
         imuIntegratorOpt_->resetIntegrationAndSetBias(prevBias_);
         // check optimization
